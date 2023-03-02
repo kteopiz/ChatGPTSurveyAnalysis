0b1b2b22341691741d19817348d45aa5c6d3c8c1
kteopiz <114126683+kteopiz@users.noreply.github.com>
2023-03-02 09:06:43 -0500

    Fixed variable names for readability

--- graphProduction.py
+++ graphProduction.py
@@ -4,13 +4,12 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-rows = []
 # Simple Printer without formatting
 with open('chatGPT_ethics_survey_responses.csv', newline='') as f:
-    reader = csv.reader(f)
+    survey_response_data = csv.reader(f)
 
     # Slice occurs at [1:] to remove the first unneeded row which contains the survey questions
-    survey_response_rows = (list(reader)[1:])
+    survey_response_rows = (list(survey_response_data)[1:])
 
 # Filters out the Time Stamps from each row of responses as it is uneeded data
 for i in range(len(survey_response_rows)):
