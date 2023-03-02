10e81ec7ad9222632a609c4df488201bb457e7ae
kteopiz <114126683+kteopiz@users.noreply.github.com>
2023-03-02 10:16:51 -0500

    Update graphProduction.py
    corrected spelling error

--- graphProduction.py
+++ graphProduction.py
@@ -13,7 +13,7 @@ with open('chatGPT_ethics_survey_responses.csv', newline='') as f:
     # Slice occurs at [1:] to remove the first unneeded row which contains the survey questions
     survey_response_rows = (list(reader)[1:])
 
-# Filters out the Time Stamps from each row of responses as it is uneeded data
+# Filters out the Time Stamps from each row of responses as it is unneeded data
 for i in range(len(survey_response_rows)):
     survey_response_rows[i] = survey_response_rows[i][1:]
 
