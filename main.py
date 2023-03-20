e9c71d606813ffa7e32d0ffd9880adc480100253
kteopiz <114126683+kteopiz@users.noreply.github.com>
2023-03-20 13:17:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from graphProduction import *
 
-# Menu needed to make figures easier to access.
+# make a menu pls, it will make accessing and calling functions so much easier
+# API type shit.
+# In order to loop the menu -> might have to return a different value than "Display Closed"
